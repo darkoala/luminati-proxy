77d9274b930c6cf280aafa278f810dd9e1304301
Lee Elenbaas <lee@hola.org>
2016-05-23 10:35:47 +0300

    run a new proxy from UI - not persistent

--- bin/public/app.js
+++ bin/public/app.js
@@ -1,2 +1,30 @@
 $(function(){
+    'use strict'; /*jslint browser:true*/
+    function formData($form){
+        var dataArr = $form.serializeArray();
+        data = {};
+        dataArr.forEach(function(item){
+            data[item.name]=item.value;
+        });
+        return data;
+    }
+
+    var $new_proxy = $('#new_proxy');
+    var $new_proxy_btn = $new_proxy.find('.btn-primary');
+    var $new_proxy_form = $new_proxy.find('form');
+    $new_proxy_btn.click(function(){
+        $new_proxy_btn.text('Saving...');
+        $.post({
+            url: 'add_proxy',
+            data: JSON.stringify(formData($new_proxy_form)),
+            contentType: 'application/json'
+        }).done(function(res){
+            // $new_proxy_btn.text('Save');
+            // $new_proxy.modal('hide');
+            location.reload();
+        }).fail(function(res){
+            $new_proxy_btn.text('Save');
+            console.log(res); // XXX lee: show error
+        });
+    });
 });
